56196ce61619b7077811ee6f5b509f78e42f5e0b
Andrew Rozhenko <andrew.r224@gmail.com>
2018-03-23 13:41:08 +0200

    Clean project after scaffolding project

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,5 +8,5 @@ RSpec.configure do |config|
   end
 
   config.shared_context_metadata_behavior = :apply_to_host_groups
-  config.order = :random
+  config.order                            = :random
 end
