67557378d8ed402a3cc8f3c37d29cdbd94165b29
Andrew Rozhenko <andrew.r224@gmail.com>
2018-03-27 14:12:39 +0300

    Add email configuration :mailbox

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -13,7 +13,20 @@ Rails.application.configure do
 
   config.log_tags = %i[request_id]
 
-  config.action_mailer.perform_caching = false
+  config.action_mailer.default_url_options   = { host: 'http://localhost:3000' }
+  config.action_mailer.delivery_method       = :smtp
+  config.action_mailer.perform_deliveries    = true
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default               charset: 'utf-8'
+  config.action_mailer.smtp_settings         = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'example.com',
+    user_name:            ENV['GMAIL_ACCOUNT'],
+    password:             ENV['GMAIL_PASSWORD'],
+    authentication:       'plain',
+    enable_starttls_auto: true
+  }
 
   config.i18n.fallbacks = true
 
